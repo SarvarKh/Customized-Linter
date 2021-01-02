2177fc544ffe29ce251e1eeb623a7f9d83cd4b35
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2021-01-02 17:44:27 +0500

    Add checker_indentation method

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,6 @@
 class JSLinter
   def initialize(file)
+    @file = file
     puts 'JS_Linter is being initialized... '
     @arr_file = File.foreach(file).to_a
   end
@@ -8,8 +9,19 @@ class JSLinter
     @arr_file
   end
 
+  # check for errors
+  # check for extra space in the code in one line
+  def checker_indentation
+    @arr_file.each_with_index do |str, index|
+      if str.count(' ') > 1
+        puts "[File: #{@file}], [Line: ##{index}], [Error: Redundant space in a string]."
+      end
+    end
+  end
+
+  # process the linter test
   def process
-    read_file
+    checker_indentation
   end
 end
 
--- samples/javascript_test_01.js
+++ samples/javascript_test_01.js
@@ -1,4 +1,4 @@
-function myFunction (p1,p2){
+ function myFunction (p1,p2){  
   console.log("some random text, bla bla bla");
   return p1 * p2;
 }
