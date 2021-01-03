b6c05d2256cd998a036150aa34e2077ffee2f0bd
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2021-01-03 20:26:29 +0500

    Fix linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,5 @@
+# JS linter class with multiple linter check methods
+
 class JSLinter
   def initialize(file)
     @file = file
@@ -12,7 +14,7 @@ class JSLinter
   # check for errors in function
   def checker_indentation
     @arr_file.each_with_index do |str, index|
-      if str[0,1].match(/\S/) && str.match('  ')
+      if str[0, 1].match(/\S/) && str.match('  ')
         puts "[File: #{@file}], [Line: ##{index + 1}], [Error: Redundant space]."
       end
     end
@@ -28,15 +30,15 @@ class JSLinter
 
   def checker_function_name
     @arr_file.each_with_index do |str, index|
-      if str.match(/function/) && !!(str.match(/[_]/))
-        puts "[File: #{@file}], [Line: ##{index + 1}], [Style Error: Prefer CamelCase over snake_case in function declaration]."
+      if str.match(/function/) && str.match(/_/)
+        puts "[File: #{@file}], [Line: ##{index + 1}], [Style Error: Prefer CamelCase over snake_case]."
       end
     end
   end
 
   def checker_parentheses_function
     @arr_file.each_with_index do |str, index|
-      if str.match(/function/) && !(str.match(/[()]/))
+      if str.match(/function/) && !str.match(/[()]/)
         puts "[File: #{@file}], [Line: ##{index + 1}], [Error: Missing parentheses]."
       end
     end
@@ -44,7 +46,7 @@ class JSLinter
 
   def checker_open_curlybrace_function
     @arr_file.each_with_index do |str, index|
-      if str.match(/function/) && !(str.match(/[{]/))
+      if str.match(/function/) && !str.match(/[{]/)
         puts "[File: #{@file}], [Line: ##{index + 1}], [Error: Missing opening curly brace]."
       end
     end
@@ -52,7 +54,7 @@ class JSLinter
 
   def checker_close_curlybrace_function
     @arr_file.each_with_index do |str, index|
-      if str.match(/function/) && !(@arr_file.any? {|n| n.match?(/[}]/)})
+      if str.match(/function/) && !(@arr_file.any? { |n| n.match?(/[}]/) })
         puts "[File: #{@file}], [Line: ##{index + 1}], [Error: Missing closing curly brace for opened curly brace]."
       end
     end
--- spec/linter_spec.rb
+++ spec/linter_spec.rb
@@ -1,12 +1,14 @@
-require_relative '../bin/main.rb'
+require_relative '../bin/main'
 
 describe JSLinter do
   source_file = '../samples/javascript_test_01.js'
   my_jslinter = JSLinter.new(source_file)
-  
+
   describe 'read_file' do
     it 'returns source_file content ' do
-      expect(my_jslinter.read_file).to eql(["function my_function   \n", "  console.log(\"some random text, bla bla bla\");\n", "  return p1 * p2; \n", "}\n"])
+      expect(my_jslinter.read_file).to eql(["function my_function   \n",
+                                            "  console.log(\"some random text, bla bla bla\");\n",
+                                            "  return p1 * p2; \n", "}\n"])
     end
   end
 end
