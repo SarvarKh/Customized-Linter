bf9fcb8042801302b6e769e7f56e8423b5cf84e9
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2021-01-04 18:39:14 +0500

    Finish moving methods from Models into ErrorScanner class

--- bin/main.rb
+++ bin/main.rb
@@ -1,34 +1,23 @@
 # JS linter class with multiple linter check methods
 require 'cli-colorize'
-require_relative '../lib/indentation'
-require_relative '../lib/indentation_end'
-require_relative '../lib/function_name'
-require_relative '../lib/parentheses_function'
-require_relative '../lib/open_curlybrace_function'
-require_relative '../lib/close_curlybrace_function'
+require_relative '../lib/error_scanner'
 
 class JSLinter
   def initialize(file)
     @file = file
     puts 'JS_Linter is being initialized... '
     @arr_file = File.foreach(@file).to_a
+    @error_scanner = ErrorScanner.new(@arr_file)
   end
-  
-  include Indentation
-  include IndentationEnd
-  include FunctionName
-  include ParenthesesFunction
-  include OpenCurlybrace
-  include CloseCurlybrace
   
   # process the linter test
   def process
-    checker_indentation
-    checker_indentation_end
-    checker_function_name
-    checker_parentheses_function
-    checker_open_curlybrace_function
-    checker_close_curlybrace_function
+    @error_scanner.checker_indentation
+    @error_scanner.checker_indentation_end
+    @error_scanner.checker_function_name
+    @error_scanner.checker_parentheses_function
+    @error_scanner.checker_open_curlybrace_function
+    @error_scanner.checker_close_curlybrace_function
   end
 end
 
