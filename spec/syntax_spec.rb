371203325f775454cfa8667bff42570045396411
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2021-01-04 23:38:00 +0500

    Split linter into indentation and syntax files

--- spec/syntax_spec.rb
+++ spec/syntax_spec.rb
@@ -4,20 +4,6 @@ source_file = './samples/javascript_test_01.js'
 describe ErrorScanner do
   error_scanner = ErrorScanner.new(source_file)
 
-  describe 'checker_indentation' do
-    it 'returns [Error: Redundant space] ' do
-      actual = '[File: ./samples/javascript_test_01.js], [Line: #1], [Error: Redundant space].'
-      expect(error_scanner.checker_indentation).to eql(actual)
-    end
-  end
-
-  describe 'checker_indentation_end' do
-    it 'returns [Error: Extra space at the end of the line] ' do
-      actual = '[File: ./samples/javascript_test_01.js], [Line: #1], [Error: Extra space at the end of the line].'
-      expect(error_scanner.checker_indentation_end).to eql(actual)
-    end
-  end
-
   describe 'checker_function_name' do
     it 'returns [Style Error: Prefer CamelCase over snake_case] ' do
       actual = '[File: ./samples/javascript_test_01.js], [Line: #1], [Style Error: Prefer CamelCase over snake_case].'
