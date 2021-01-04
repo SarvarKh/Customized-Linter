11e5addc43c8856291e12fe9fcc6b4dc3c4b6e4d
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2021-01-04 13:11:21 +0500

    Convert all error reporting colorful with colorize gem

--- bin/main.rb
+++ bin/main.rb
@@ -24,7 +24,7 @@ class JSLinter
   def checker_indentation_end
     @arr_file.each_with_index do |str, index|
       if str[str.length - 2] == ' '
-        puts "[File: #{@file}], [Line: ##{index + 1}], [Error: Extra space at the end of the line]."
+        puts "[File: #{@file}], " + "[Line: ##{index + 1}], ".blue + '[Error: Extra space at the end of the line].'.red
       end
     end
   end
@@ -32,7 +32,7 @@ class JSLinter
   def checker_function_name
     @arr_file.each_with_index do |str, index|
       if str.match(/function/) && str.match(/_/)
-        puts "[File: #{@file}], [Line: ##{index + 1}], [Style Error: Prefer CamelCase over snake_case]."
+        puts "[File: #{@file}], " + "[Line: ##{index + 1}], ".blue + '[Style Error: Prefer CamelCase over snake_case].'.red
       end
     end
   end
@@ -40,7 +40,7 @@ class JSLinter
   def checker_parentheses_function
     @arr_file.each_with_index do |str, index|
       if str.match(/function/) && !str.match(/[()]/)
-        puts "[File: #{@file}], [Line: ##{index + 1}], [Error: Missing parentheses]."
+        puts "[File: #{@file}], " + "[Line: ##{index + 1}], ".blue + '[Error: Missing parentheses].'.red
       end
     end
   end
@@ -48,7 +48,7 @@ class JSLinter
   def checker_open_curlybrace_function
     @arr_file.each_with_index do |str, index|
       if str.match(/function/) && !str.match(/[{]/)
-        puts "[File: #{@file}], [Line: ##{index + 1}], [Error: Missing opening curly brace]."
+        puts "[File: #{@file}], " + "[Line: ##{index + 1}], ".blue + '[Error: Missing opening curly brace].'.red
       end
     end
   end
@@ -56,7 +56,7 @@ class JSLinter
   def checker_close_curlybrace_function
     @arr_file.each_with_index do |str, index|
       if str.match(/function/) && !(@arr_file.any? { |n| n.match?(/[}]/) })
-        puts "[File: #{@file}], [Line: ##{index + 1}], [Error: Missing closing curly brace for opened curly brace]."
+        puts "[File: #{@file}], " + "[Line: ##{index + 1}], ".blue + '[Error: Missing closing curly brace for opened curly brace].'.red
       end
     end
   end
