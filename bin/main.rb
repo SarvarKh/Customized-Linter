3f6c840b8d6d3cf4fd30e7a22d4dcc9a5a493a9a
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2021-01-04 14:14:40 +0500

    Remove duplicated method close_curlybrace from main file

--- bin/main.rb
+++ bin/main.rb
@@ -21,14 +21,6 @@ class JSLinter
   include OpenCurlybrace
   include CloseCurlybrace
   
-  def checker_close_curlybrace_function
-    @arr_file.each_with_index do |str, index|
-      if str.match(/function/) && !(@arr_file.any? { |n| n.match?(/[}]/) })
-        puts "[File: #{@file}], " + "[Line: ##{index + 1}], ".blue + '[Error: Missing closing curly brace for opened curly brace].'.red
-      end
-    end
-  end
-
   # process the linter test
   def process
     checker_indentation
