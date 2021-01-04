4fe0844f3c323cede4d007dab34444b3da70b221
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2021-01-04 19:16:05 +0500

    Add rspec test for checker_indentation

--- lib/error_scanner.rb
+++ lib/error_scanner.rb
@@ -6,7 +6,7 @@ class ErrorScanner
   def checker_indentation
     @arr_file.each_with_index do |str, index|
       if str[0, 1].match(/\S/) && str.match('  ')
-        puts "[File: #{@file}], " + "[Line: ##{index + 1}], ".blue + '[Error: Redundant space].'.red
+        puts "[File: #{@file}], " + "[Line: ##{index + 1}], " + '[Error: Redundant space].'
       end
     end
   end
--- spec/linter_spec.rb
+++ spec/linter_spec.rb
@@ -1,20 +1,14 @@
-require_relative '../bin/main'
+require_relative '../lib/error_scanner.rb'
 
-describe JSLinter do
-  source_file = '../samples/javascript_test_01.js'
-  my_jslinter = JSLinter.new(source_file)
-
-  describe 'read_file' do
-    it 'returns source_file content ' do
-      expect(my_jslinter.read_file).to eql(["function my_function   \n",
-                                            "  console.log(\"some random text, bla bla bla\");\n",
-                                            "  return p1 * p2; \n", "}\n"])
-    end
-  end
+describe ErrorScanner do
+  source_file = ["function my_function   \n", "  console.log(\"some random text, bla bla bla\");\n", "  return p1 * p2; \n", "\n"]
+  error_scanner = ErrorScanner.new(source_file)
 
   describe 'checker_indentation' do
     it 'returns [Error: Redundant space] ' do
-      expect(my_jslinter.checker_indentation).to eql('[File: ./samples/javascript_test_01.js], [Line: #1], [Error: Redundant space].')
+      expectation = error_scanner.checker_indentation
+      actual = '[File: ./samples/javascript_test_01.js], [Line: #1], [Error: Redundant space].'
+      expect(expectation).to eql(actual)
     end
   end
 end
